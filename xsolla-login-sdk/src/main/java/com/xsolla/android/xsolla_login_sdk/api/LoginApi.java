9639cd7d10755e7baeb138a6eb517cf28036e082
ratanoff <leonid.ratanoff@gmail.com>
2020-01-15 22:31:59 +0500

    SDK-220 JWT Auth method implementation

--- xsolla-login-sdk/src/main/java/com/xsolla/android/xsolla_login_sdk/api/LoginApi.java
+++ xsolla-login-sdk/src/main/java/com/xsolla/android/xsolla_login_sdk/api/LoginApi.java
@@ -1,6 +1,8 @@
 package com.xsolla.android.xsolla_login_sdk.api;
 
+import com.xsolla.android.xsolla_login_sdk.entity.request.LoginUser;
 import com.xsolla.android.xsolla_login_sdk.entity.request.NewUser;
+import com.xsolla.android.xsolla_login_sdk.entity.response.LoginResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -11,4 +13,7 @@ public interface LoginApi {
 
     @POST("/api/user")
     Call<Void> registerUser(@Query("projectId") String projectId, @Body NewUser newUser);
+
+    @POST("/api/login")
+    Call<LoginResponse> login(@Query("projectId") String projectId, @Body LoginUser loginUser);
 }
