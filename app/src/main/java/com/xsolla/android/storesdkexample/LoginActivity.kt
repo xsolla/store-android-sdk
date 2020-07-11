7dd057bdc506265df6800f0a1984016cda02c0af
ratanoff <leonid.ratanoff@gmail.com>
2020-07-11 17:36:10 +0500

    SDK-611 add logo and tabs

---
+++ app/src/main/java/com/xsolla/android/storesdkexample/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.xsolla.android.storesdkexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+
+
+    private fun initTabs() {
+
+    }
+
+}
