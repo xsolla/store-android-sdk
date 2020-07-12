2ac2b66f7fdba95ee4e1c32ed8970d8bec6605b4
ratanoff <leonid.ratanoff@gmail.com>
2020-07-12 18:09:45 +0500

    SDK-611 Sign Up Fragment

---
+++ app/src/main/java/com/xsolla/android/storesdkexample/fragments/LoginFragment.kt
@@ -0,0 +1,15 @@
+package com.xsolla.android.storesdkexample.fragments
+
+import com.xsolla.android.storesdkexample.R
+import com.xsolla.android.storesdkexample.fragments.base.BaseFragment
+
+class LoginFragment : BaseFragment() {
+
+    override fun getLayout(): Int {
+        return R.layout.fragment_login
+    }
+
+    override fun initUI() {
+
+    }
+}
---
+++ app/src/main/java/com/xsolla/android/storesdkexample/fragments/SignUpFragment.kt
@@ -0,0 +1,15 @@
+package com.xsolla.android.storesdkexample.fragments
+
+import com.xsolla.android.storesdkexample.R
+import com.xsolla.android.storesdkexample.fragments.base.BaseFragment
+
+class SignUpFragment : BaseFragment() {
+
+    override fun getLayout(): Int {
+        return R.layout.fragment_sign_up
+    }
+
+    override fun initUI() {
+
+    }
+}
