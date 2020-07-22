616a1dbdb1db97ae448543e7659af5710939de95
ratanoff <leonid.ratanoff@gmail.com>
2020-07-22 21:31:24 +0500

    SDK-648 Uniform style for catalog item for all screens

--- app/src/main/java/com/xsolla/android/storesdkexample/ui/ViPageFragment.kt
+++ app/src/main/java/com/xsolla/android/storesdkexample/ui/ViPageFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import com.xsolla.android.store.entity.response.items.VirtualItemsResponse
@@ -43,7 +45,11 @@ class ViPageFragment : Fragment(), PurchaseListener {
         val items = requireArguments().getParcelable<BaseParcelable>(ARG_ITEMS)?.value as? List<VirtualItemsResponse.Item>
         items?.let {
             with(view.catalogRecyclerView) {
-                layoutManager = LinearLayoutManager(view.context)
+                val linearLayoutManager = LinearLayoutManager(context)
+                addItemDecoration(DividerItemDecoration(context, linearLayoutManager.orientation).apply {
+                    ContextCompat.getDrawable(context, R.drawable.item_divider)?.let { setDrawable(it) }
+                })
+                layoutManager = linearLayoutManager
                 adapter = ViAdapter(it, vmCart, vmBalance, this@ViPageFragment)
             }
         }
