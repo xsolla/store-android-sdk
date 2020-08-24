e93672235731a85b762d2e1900ba1976f813d13f
a.ilkaev <AC3E6Tbw55p3uYHeQheT>
2020-08-24 10:59:05 +0500

    change nullability with API doc

--- xsolla-store-sdk/src/main/java/com/xsolla/android/store/entity/request/inventory/ConsumeItemBody.kt
+++ xsolla-store-sdk/src/main/java/com/xsolla/android/store/entity/request/inventory/ConsumeItemBody.kt
@@ -3,8 +3,8 @@ package com.xsolla.android.store.entity.request.inventory
 import com.google.gson.annotations.SerializedName
 
 data class ConsumeItemBody(
-    val sku: String? = null,
-    val quantity: Int,
+    val sku: String,
+    val quantity: Int? = null,
     @SerializedName("instance_id")
     val instanceId: String? = null
 )
