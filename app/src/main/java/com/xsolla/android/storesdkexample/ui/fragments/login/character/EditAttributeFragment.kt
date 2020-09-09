26f4372903c896213793a4940c2fd192dae5a688
a.ilkaev <AC3E6Tbw55p3uYHeQheT>
2020-09-09 19:02:36 +0500

    add edit attribute fragment

---
+++ app/src/main/java/com/xsolla/android/storesdkexample/ui/fragments/login/character/EditAttributeFragment.kt
@@ -0,0 +1,14 @@
+package com.xsolla.android.storesdkexample.ui.fragments.login.character
+
+import androidx.navigation.fragment.navArgs
+import com.xsolla.android.storesdkexample.R
+import com.xsolla.android.storesdkexample.ui.fragments.base.BaseFragment
+
+class EditAttributeFragment : BaseFragment() {
+
+    override fun getLayout() = R.layout.fragment_edit_attribute
+
+    override fun initUI() {
+
+    }
+}
