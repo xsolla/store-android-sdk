0c656d447d6cc67c3c7f81180f37a9c91236849d
a.ilkaev <AC3E6Tbw55p3uYHeQheT>
2020-09-18 16:52:16 +0500

    add custom_parameters parameter to CreateOrderRequestBody

--- xsolla-store-sdk/src/main/java/com/xsolla/android/store/entity/request/payment/CreateOrderRequestBody.kt
+++ xsolla-store-sdk/src/main/java/com/xsolla/android/store/entity/request/payment/CreateOrderRequestBody.kt
@@ -1,13 +1,18 @@
 package com.xsolla.android.store.entity.request.payment
 
-data class CreateOrderRequestBody(
+import com.google.gson.annotations.SerializedName
+
+class CreateOrderRequestBody private constructor(
     val currency: String = "USD",
     val locale: String = "en",
-    val sandbox: Boolean = true
+    val sandbox: Boolean = true,
+    @SerializedName("custom_parameters")
+    val customParameters: Any? = null
 ) {
     constructor(options: PaymentOptions?) : this(
         options?.currency ?: "USD",
         options?.locale ?: "en",
-        options?.isSandbox ?: true
+        options?.isSandbox ?: true,
+        options?.customParameters?.toJsonObject()
     )
 }
--- xsolla-store-sdk/src/main/java/com/xsolla/android/store/entity/request/payment/PaymentOptions.kt
+++ xsolla-store-sdk/src/main/java/com/xsolla/android/store/entity/request/payment/PaymentOptions.kt
@@ -1,7 +1,61 @@
 package com.xsolla.android.store.entity.request.payment
 
+import com.google.gson.JsonObject
+
 data class PaymentOptions(
     val currency: String = "USD",
     val locale: String = "en",
-    val isSandbox: Boolean = true
+    val isSandbox: Boolean = true,
+    val customParameters: CustomParameters? = null
 )
+
+class CustomParameters private constructor(private val parameters: Map<String, Value>) {
+    class Builder {
+        private val parameters = mutableMapOf<String, Value>()
+
+        fun addParam(key: String, value: Value): Builder {
+            if (key.length !in 1..255) {
+                throw CustomParametersException("Custom parameter key length must be from 1 to 255")
+            }
+
+            parameters[key] = value
+            return this
+        }
+
+        fun build(): CustomParameters {
+            if (parameters.size !in 1..200) {
+                throw CustomParametersException("Custom parameters must be from 1 to 200")
+            }
+
+            return CustomParameters(parameters)
+        }
+    }
+
+    sealed class Value(open val value: Any) {
+        data class String(override val value: kotlin.String) : Value(value)
+        data class Number(override val value: kotlin.Number) : Value(value)
+        data class Boolean(override val value: kotlin.Boolean) : Value(value)
+    }
+
+    fun toJsonObject() =
+        JsonObject().apply {
+            parameters.forEach { (key, value) ->
+                addProperty(key, value, this)
+            }
+        }
+
+    private fun addProperty(key: String, value: Value, jsonObject: JsonObject) =
+        when (value) {
+            is Value.String -> {
+                jsonObject.addProperty(key, value.value)
+            }
+            is Value.Boolean -> {
+                jsonObject.addProperty(key, value.value)
+            }
+            is Value.Number -> {
+                jsonObject.addProperty(key, value.value)
+            }
+        }
+
+    private class CustomParametersException(message: String) : RuntimeException(message)
+}
