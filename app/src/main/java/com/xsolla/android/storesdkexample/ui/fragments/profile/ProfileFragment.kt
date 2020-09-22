2c6009e77056f51ee6730e0f39226b7eb3ec76eb
a.ilkaev <AC3E6Tbw55p3uYHeQheT>
2020-09-22 18:50:44 +0500

    add fragment, ui

---
+++ app/src/main/java/com/xsolla/android/storesdkexample/ui/fragments/profile/ProfileFragment.kt
@@ -0,0 +1,12 @@
+package com.xsolla.android.storesdkexample.ui.fragments.profile
+
+import com.xsolla.android.storesdkexample.R
+import com.xsolla.android.storesdkexample.ui.fragments.base.BaseFragment
+
+class ProfileFragment : BaseFragment() {
+    override fun getLayout() = R.layout.fragment_profile
+
+    override fun initUI() {
+        TODO("Not yet implemented")
+    }
+}
