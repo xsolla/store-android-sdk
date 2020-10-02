08fbbebc436a09fe2925791c7885a4536b67c91b
a.ilkaev <AC3E6Tbw55p3uYHeQheT>
2020-10-02 17:25:48 +0500

    SDK-731 update logic for sending request for update

--- app/src/main/java/com/xsolla/android/storesdkexample/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/xsolla/android/storesdkexample/ui/fragments/profile/ProfileFragment.kt
@@ -8,21 +8,24 @@ import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.google.android.material.textfield.TextInputLayout
 import com.xsolla.android.storesdkexample.R
 import com.xsolla.android.storesdkexample.ui.fragments.base.BaseFragment
-import com.xsolla.android.storesdkexample.ui.fragments.login.ResetPasswordFragmentDirections
 import com.xsolla.android.storesdkexample.ui.vm.FieldsForChanging
 import com.xsolla.android.storesdkexample.ui.vm.Gender
+import com.xsolla.android.storesdkexample.ui.vm.UserDetailsUi
+import com.xsolla.android.storesdkexample.ui.vm.ValidateFieldResult
 import com.xsolla.android.storesdkexample.ui.vm.VmProfile
 import kotlinx.android.synthetic.main.activity_store.appbar
 import kotlinx.android.synthetic.main.app_bar_main.view.balanceLayout
 import kotlinx.android.synthetic.main.app_bar_main.view.mainToolbar
 import kotlinx.android.synthetic.main.fragment_profile.avatar
 import kotlinx.android.synthetic.main.fragment_profile.birthdayInput
+import kotlinx.android.synthetic.main.fragment_profile.birthdayLayout
 import kotlinx.android.synthetic.main.fragment_profile.emailInput
 import kotlinx.android.synthetic.main.fragment_profile.emailLayout
 import kotlinx.android.synthetic.main.fragment_profile.firstnameInput
@@ -38,6 +41,8 @@ import kotlinx.android.synthetic.main.fragment_profile.phoneLayout
 import kotlinx.android.synthetic.main.fragment_profile.resetPasswordButton
 import kotlinx.android.synthetic.main.fragment_profile.usernameInput
 import kotlinx.android.synthetic.main.fragment_profile.usernameLayout
+import java.util.Calendar
+import java.util.Date
 
 class ProfileFragment : BaseFragment() {
     private val viewModel: VmProfile by activityViewModels()
@@ -48,7 +53,7 @@ class ProfileFragment : BaseFragment() {
         requireActivity().appbar.balanceLayout.isVisible = false
         requireActivity().appbar.mainToolbar.menu.clear()
 
-        viewModel.error.observe(viewLifecycleOwner) {
+        viewModel.message.observe(viewLifecycleOwner) {
             showSnack(it)
         }
         viewModel.state.observe(viewLifecycleOwner) { userData ->
@@ -64,7 +69,7 @@ class ProfileFragment : BaseFragment() {
 
             // Nickname
             nickname.text = userData.nickname ?: userData.firstName ?: userData.lastName ?: "Nickname"
-            userData.nickname?.let { nicknameInput.setText(it) }
+            nicknameInput.setText(userData.nickname)
 
             // Email
             emailLayout.isVisible = userData.email != null
@@ -83,7 +88,7 @@ class ProfileFragment : BaseFragment() {
 
             // Birthday
             birthdayInput.setText(userData.birthday)
-            if (userData.birthday != null) birthdayInput.isEnabled = false
+            if (userData.birthday != null) birthdayLayout.isEnabled = false
 
             // Gender
             // https://stackoverflow.com/questions/28184543/android-autocompletetextview-not-showing-after-settext-is-called
@@ -92,14 +97,23 @@ class ProfileFragment : BaseFragment() {
             // Button
             resetPasswordButton.isVisible = userData.email != null
         }
+        viewModel.stateForChanging.observe(viewLifecycleOwner) {
+            if (it == null) {
+                // block button
+                return@observe
+            }
+            if (it == viewModel.state.value) {
+                // block button
+                return@observe
+            }
+            // unblock button
+        }
 
         viewModel.fieldChangeResult.observe(viewLifecycleOwner) {
             Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
         }
 
-        resetPasswordButton.setOnClickListener {
-            findNavController().navigate(ResetPasswordFragmentDirections.toResetPasswordFragment())
-        }
+        resetPasswordButton.setOnClickListener { viewModel.resetPassword() }
 
         avatar.setOnClickListener {
             val id = viewModel.state.value?.id ?: return@setOnClickListener
@@ -108,6 +122,28 @@ class ProfileFragment : BaseFragment() {
 
         configureBirthday()
         configureFields()
+    }
+
+    private fun configureFields() {
+        nicknameInput.addTextChangedListener {
+            viewModel.stateForChanging.value = viewModel.stateForChanging.value!!.copy(nickname = it?.toString())
+        }
+        phoneInput.addTextChangedListener {
+            viewModel.stateForChanging.value = viewModel.stateForChanging.value!!.copy(phone = it?.toString())
+        }
+        firstnameInput.addTextChangedListener {
+            viewModel.stateForChanging.value = viewModel.stateForChanging.value!!.copy(firstName = it?.toString())
+        }
+        lastnameInput.addTextChangedListener {
+            viewModel.stateForChanging.value = viewModel.stateForChanging.value!!.copy(lastName = it?.toString())
+        }
+        birthdayInput.addTextChangedListener {
+            viewModel.stateForChanging.value = viewModel.stateForChanging.value!!.copy(birthday = it?.toString())
+        }
+        genderInput.addTextChangedListener {
+            val text = it?.toString() ?: return@addTextChangedListener
+            viewModel.stateForChanging.value = viewModel.stateForChanging.value!!.copy(gender = Gender.valueOf(text))
+        }
     }
 
     override fun onDestroyView() {
@@ -131,34 +167,51 @@ class ProfileFragment : BaseFragment() {
             .into(avatar)
     }
 
-    private fun configureFields() {
-        firstnameInput.setOnFocusChangeListener { v, hasFocus -> focusListener(v, hasFocus, firstnameLayout, FieldsForChanging.FIRST_NAME) }
-        nicknameInput.setOnFocusChangeListener { v, hasFocus -> focusListener(v, hasFocus, nicknameLayout, FieldsForChanging.NICKNAME) }
-        phoneInput.setOnFocusChangeListener { v, hasFocus -> focusListener(v, hasFocus, phoneLayout, FieldsForChanging.PHONE) }
-        lastnameInput.setOnFocusChangeListener { v, hasFocus -> focusListener(v, hasFocus, lastnameLayout, FieldsForChanging.LAST_NAME) }
-        lastnameInput.setOnEditorActionListener { v, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                validateAndUpdate(lastnameLayout, FieldsForChanging.LAST_NAME, (v as? EditText)?.text?.toString())
-                true
-            }
-            false
+    private fun updateFields(newState: UserDetailsUi) {
+        hideKeyboard()
+
+        val firstnameValidation = if (newState.firstName == viewModel.state.value!!.firstName) {
+            ValidateFieldResult(true)
+        } else {
+            val validateField = viewModel.validateField(FieldsForChanging.FIRST_NAME, newState.firstName)
+            showErrorOnFieldOrClear(validateField, firstnameLayout)
+            validateField
+        }
+
+        val lastnameValidation = if (newState.lastName == viewModel.state.value!!.lastName) {
+            ValidateFieldResult(true)
+        } else {
+            val validateField = viewModel.validateField(FieldsForChanging.LAST_NAME, newState.lastName)
+            showErrorOnFieldOrClear(validateField, lastnameLayout)
+            validateField
+        }
+
+        val phoneValidation = if (newState.phone == viewModel.state.value!!.phone) {
+            ValidateFieldResult(true)
+        } else {
+            val validateField = viewModel.validateField(FieldsForChanging.PHONE, newState.phone)
+            showErrorOnFieldOrClear(validateField, phoneLayout)
+            validateField
+        }
+
+        val nicknameValidation = if (newState.nickname == viewModel.state.value!!.nickname) {
+            ValidateFieldResult(true)
+        } else {
+            val validateField = viewModel.validateField(FieldsForChanging.NICKNAME, newState.nickname)
+            showErrorOnFieldOrClear(validateField, nicknameLayout)
+            validateField
+        }
+
+        if (firstnameValidation.isSuccess && lastnameValidation.isSuccess && phoneValidation.isSuccess && nicknameValidation.isSuccess) {
+            viewModel.updateFields(newState)
         }
     }
 
-    private fun focusListener(view: View, hasFocus: Boolean, layout: TextInputLayout, field: FieldsForChanging) {
-        if (hasFocus) return
-        view as EditText
-
-        validateAndUpdate(layout, field, view.text?.toString())
-    }
-
-    private fun validateAndUpdate(layout: TextInputLayout, field: FieldsForChanging, value: String?) {
-        val validateResult = viewModel.validateField(field, value)
-        if (validateResult.isSuccess) {
-            viewModel.updateField(field, value!!)
+    private fun showErrorOnFieldOrClear(validation: ValidateFieldResult, layout: TextInputLayout) {
+        if (validation.isSuccess) {
             layout.isErrorEnabled = false
         } else {
-            layout.error = validateResult.errorMessage
+            layout.error = validation.errorMessage
         }
     }
 
@@ -172,8 +225,6 @@ class ProfileFragment : BaseFragment() {
                     val day = if (dayOfMonth < 10) "0$dayOfMonth" else "$dayOfMonth"
                     val result = "$year-$month-$day"
                     birthdayInput.setText(result)
-
-                    viewModel.updateField(FieldsForChanging.BIRTHDAY, result)
                 },
                 1990,
                 0,
@@ -181,16 +232,17 @@ class ProfileFragment : BaseFragment() {
             )
 
             dialog.datePicker.maxDate = System.currentTimeMillis()
+            dialog.datePicker.minDate = Calendar.getInstance().apply { set(1950, 0, 1) }.timeInMillis
             dialog.show()
         }
     }
 
     private fun configureGender() {
-        val items = Gender.values().map { it.name }
+        val items = Gender.values()
         val adapter = ArrayAdapter(requireContext(), R.layout.item_gender, items)
         genderInput.setAdapter(adapter)
-        genderInput.setOnItemClickListener { _, _, position, _ ->
-            viewModel.updateField(FieldsForChanging.GENDER, adapter.getItem(position)!!)
-        }
+        /*genderInput.setOnItemClickListener { _, _, position, _ ->
+
+        }*/
     }
 }
--- app/src/main/java/com/xsolla/android/storesdkexample/ui/vm/VmProfile.kt
+++ app/src/main/java/com/xsolla/android/storesdkexample/ui/vm/VmProfile.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.xsolla.android.login.XLogin
 import com.xsolla.android.login.callback.GetCurrentUserDetailsCallback
+import com.xsolla.android.login.callback.ResetPasswordCallback
 import com.xsolla.android.login.callback.UpdateCurrentUserDetailsCallback
 import com.xsolla.android.login.callback.UpdateCurrentUserPhoneCallback
 import com.xsolla.android.login.entity.response.GenderResponse
@@ -21,7 +22,9 @@ class VmProfile : ViewModel() {
     private val _state = MutableLiveData<UserDetailsUi>()
     val state: LiveData<UserDetailsUi> = _state
 
-    val error = SingleLiveEvent<String>()
+    val stateForChanging = MutableLiveData<UserDetailsUi>()
+
+    val message = SingleLiveEvent<String>()
 
     val fieldChangeResult = SingleLiveEvent<FieldChangeResult>()
 
@@ -32,7 +35,7 @@ class VmProfile : ViewModel() {
     fun load() {
         XLogin.getCurrentUserDetails(object : GetCurrentUserDetailsCallback {
             override fun onSuccess(data: UserDetailsResponse) {
-                _state.value = UserDetailsUi(
+                val uiEntity = UserDetailsUi(
                     id = data.id,
                     email = data.email,
                     username = data.username,
@@ -44,70 +47,45 @@ class VmProfile : ViewModel() {
                     gender = Gender.getBy(data.gender),
                     avatar = data.picture
                 )
+                _state.value = uiEntity
+                stateForChanging.value = uiEntity
             }
 
             override fun onError(throwable: Throwable?, errorMessage: String?) {
-                error.value = throwable?.message ?: errorMessage ?: "Failure"
+                message.value = throwable?.message ?: errorMessage ?: "Failure"
             }
         })
     }
 
-    fun updateField(field: FieldsForChanging, value: String) {
-        var state = state.value
-        if (state == null) {
-            error.value = "Field cannot be changed, state is null"
-            return
-        }
+    fun updateFields(newState: UserDetailsUi) {
+        val gender = newState.gender?.name?.toLowerCase(Locale.getDefault())?.first()?.toString()
+        XLogin.updateCurrentUserDetails(newState.birthday, newState.firstName, gender, newState.lastName, newState.nickname, object : UpdateCurrentUserDetailsCallback {
+            override fun onSuccess() {
+                message.value = "Fields were successfuly changed"
+                _state.value = newState.copy(phone = _state.value!!.phone)
 
-        when (field) {
-            FieldsForChanging.NICKNAME -> {
-                state = state.copy(nickname = value)
+                if (newState.phone != state.value!!.phone) {
+                    updatePhone(newState.phone!!)
+                }
             }
-            FieldsForChanging.FIRST_NAME -> {
-                state = state.copy(firstName = value)
-            }
-            FieldsForChanging.LAST_NAME -> {
-                state = state.copy(lastName = value)
-            }
-            FieldsForChanging.GENDER -> {
-                state = state.copy(gender = if (value.startsWith("M", ignoreCase = true)) Gender.Male else Gender.Female)
-            }
-            FieldsForChanging.BIRTHDAY -> {
-                state = state.copy(birthday = value)
-            }
-            FieldsForChanging.PHONE -> {
-                state = state.copy(phone = value)
-            }
-        }
 
-        if (field == FieldsForChanging.PHONE) {
-            XLogin.updateCurrentUserPhone(state.phone, object : UpdateCurrentUserPhoneCallback {
-                override fun onSuccess() {
-                    val result = FieldChangeResult(field, "${field.name} was successfully changed")
-                    fieldChangeResult.value = result
-                    _state.value = state
-                }
+            override fun onError(throwable: Throwable?, errorMessage: String?) {
+                message.value = throwable?.message ?: errorMessage ?: "Failure"
+            }
+        })
+    }
 
-                override fun onError(throwable: Throwable?, errorMessage: String?) {
-                    val result = FieldChangeResult(field, throwable?.message ?: errorMessage ?: "Failure")
-                    fieldChangeResult.value = result
-                }
-            })
-        } else {
-            val gender = state.gender?.name?.toLowerCase(Locale.getDefault())?.first()?.toString()
-            XLogin.updateCurrentUserDetails(state.birthday, state.firstName, gender, state.lastName, state.nickname, object : UpdateCurrentUserDetailsCallback {
-                override fun onSuccess() {
-                    val result = FieldChangeResult(field, "${field.name} was successfully changed")
-                    fieldChangeResult.value = result
-                    _state.value = state
-                }
+    fun updatePhone(newPhone: String) {
+        XLogin.updateCurrentUserPhone(newPhone, object : UpdateCurrentUserPhoneCallback {
+            override fun onSuccess() {
+                message.value = "Fields were successfuly changed"
+                _state.value = state.value!!.copy(phone = newPhone)
+            }
 
-                override fun onError(throwable: Throwable?, errorMessage: String?) {
-                    val result = FieldChangeResult(field, throwable?.message ?: errorMessage ?: "Failure")
-                    fieldChangeResult.value = result
-                }
-            })
-        }
+            override fun onError(throwable: Throwable?, errorMessage: String?) {
+                message.value = throwable?.message ?: errorMessage ?: "Failure"
+            }
+        })
     }
 
     fun validateField(field: FieldsForChanging, text: String?): ValidateFieldResult {
@@ -133,19 +111,33 @@ class VmProfile : ViewModel() {
     fun updateAvatar(avatar: String?) {
         _state.value = _state.value?.copy(avatar = avatar)
     }
+
+    fun resetPassword() {
+        val username = state.value!!.username ?: return
+        val email = state.value!!.email ?: return
+        XLogin.resetPassword(username, object : ResetPasswordCallback {
+            override fun onSuccess() {
+                message.value = "A letter has been sent to the $email. Follow the link in the letter — and you can create a new password"
+            }
+
+            override fun onError(throwable: Throwable?, errorMessage: String?) {
+                message.value = throwable?.message ?: errorMessage ?: "Failure"
+            }
+        })
+    }
 }
 
 data class UserDetailsUi(
-    val id: String,
-    val email: String?,
-    val username: String?,
-    val nickname: String?,
-    val firstName: String?,
-    val lastName: String?,
-    val birthday: String?,
-    val phone: String?,
-    val gender: Gender?,
-    val avatar: String?
+    val id: String = "",
+    val email: String? = null,
+    val username: String? = null,
+    val nickname: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val birthday: String? = null,
+    val phone: String? = null,
+    val gender: Gender? = null,
+    val avatar: String? = null
 )
 
 data class FieldChangeResult(val field: FieldsForChanging, val message: String)
@@ -180,5 +172,5 @@ enum class Gender(val response: GenderResponse) {
 
 data class ValidateFieldResult(
     val isSuccess: Boolean,
-    val errorMessage: String?
+    val errorMessage: String? = null
 )
