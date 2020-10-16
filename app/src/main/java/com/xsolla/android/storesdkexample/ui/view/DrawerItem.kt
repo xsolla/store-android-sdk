afd4cc13b7afce07a0bea7fc25d61b8ea9b43142
l.ratanov <l.ratanov@xsolla.com>
2020-10-16 17:02:21 +0500

    SDK-814 Use TypedArray extension

--- app/src/main/java/com/xsolla/android/storesdkexample/ui/view/DrawerItem.kt
+++ app/src/main/java/com/xsolla/android/storesdkexample/ui/view/DrawerItem.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import androidx.core.content.res.use
 import com.xsolla.android.storesdkexample.R
 import kotlinx.android.synthetic.main.item_drawer.view.*
 
@@ -12,17 +13,9 @@ class DrawerItem(context: Context, attrs: AttributeSet): LinearLayout(context, a
     init {
         LayoutInflater.from(context).inflate(R.layout.item_drawer, this, true)
 
-        context.theme.obtainStyledAttributes(
-                attrs,
-                R.styleable.DrawerItem,
-                0, 0
-        ).apply {
-            try {
-                item_icon.setImageResource(getResourceId(R.styleable.DrawerItem_item_icon, 0))
-                item_text.text = getString(R.styleable.DrawerItem_item_text)
-            } finally {
-                recycle()
-            }
+        context.theme.obtainStyledAttributes(attrs, R.styleable.DrawerItem, 0, 0).use {
+            item_icon.setImageResource(it.getResourceId(R.styleable.DrawerItem_item_icon, 0))
+            item_text.text = it.getString(R.styleable.DrawerItem_item_text)
         }
     }
 
