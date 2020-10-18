322d82a175c97e05b7d32c14e806b42eec3587b9
a.ilkaev <AC3E6Tbw55p3uYHeQheT>
2020-10-18 20:45:11 +0500

    SDK-812 add navigating in coupons

--- app/src/main/java/com/xsolla/android/storesdkexample/ui/fragments/store/RedeemCouponFragment.kt
+++ app/src/main/java/com/xsolla/android/storesdkexample/ui/fragments/store/RedeemCouponFragment.kt
@@ -5,13 +5,16 @@ import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
 import com.xsolla.android.storesdkexample.R
 import com.xsolla.android.storesdkexample.ui.fragments.base.BaseFragment
 import com.xsolla.android.storesdkexample.ui.vm.VmCoupon
 import kotlinx.android.synthetic.main.activity_store.appbar
 import kotlinx.android.synthetic.main.app_bar_main.view.toolbar
+import kotlinx.android.synthetic.main.fragment_redeem_coupon.cancelButton
 import kotlinx.android.synthetic.main.fragment_redeem_coupon.couponInput
 import kotlinx.android.synthetic.main.fragment_redeem_coupon.redeemButton
+import kotlinx.android.synthetic.main.fragment_redeem_coupon.toolbar
 
 class RedeemCouponFragment : BaseFragment() {
 
@@ -30,6 +33,8 @@ class RedeemCouponFragment : BaseFragment() {
         }
 
         redeemButton.setOnClickListener { viewModel.redeemCoupon(couponInput.text.toString()) }
+        cancelButton.setOnClickListener { findNavController().navigateUp() }
+        toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
     }
 
     override fun onDestroyView() {
