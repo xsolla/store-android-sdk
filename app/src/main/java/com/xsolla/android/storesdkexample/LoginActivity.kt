e87a5cb8983d6b0ba9274d063eb3f25bc731b999
a.ilkaev <AC3E6Tbw55p3uYHeQheT>
2020-12-01 16:07:19 +0500

    SDK-911 fix native auth callback for onActivityResult

--- app/src/main/java/com/xsolla/android/storesdkexample/LoginActivity.kt
+++ app/src/main/java/com/xsolla/android/storesdkexample/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.xsolla.android.storesdkexample
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,6 +9,16 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        supportFragmentManager.fragments.forEach { fragment ->
+            fragment.onActivityResult(requestCode, resultCode, data)
+            fragment.childFragmentManager.fragments.forEach { childFragment ->
+                childFragment.onActivityResult(requestCode, resultCode, data)
+            }
+        }
     }
 
 }
