9291f447ae200c784bc0b78c6d5aa22e73a2235f
a.nikolaev <a.nikolaev@xsolla.com>
2021-06-24 18:15:25 +0500

    add tests to store sdk, fix payment options

--- xsolla-inventory-sdk/src/test/java/com/xsolla/android/inventory/TestParams.kt
+++ xsolla-inventory-sdk/src/test/java/com/xsolla/android/inventory/TestParams.kt
@@ -1,7 +1,7 @@
 package com.xsolla.android.inventory
 
 const val projectId = 53916
-const val userToken = "msp5krOJHpWcP3qRB7vhZldyJj1W45ao"
+const val userToken = "msp59DY6N7yn0im8zJTJt6UcOYeO50lZ"
 
 const val subscriptionSku = "premium_access"
 const val itemForConsume = "airplane"
---
+++ xsolla-store-sdk/src/test/java/com/xsolla/android/store/TestParams.kt
@@ -0,0 +1,6 @@
+package com.xsolla.android.store
+
+const val projectId = 53916
+const val userToken = "msp59DY6N7yn0im8zJTJt6UcOYeO50lZ"
+
+const val itemForOrderBySku = "airplane"
