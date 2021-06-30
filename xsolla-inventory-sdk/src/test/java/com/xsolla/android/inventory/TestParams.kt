d96937ee0474311ded840eb75e37f48578c7be1a
a.nikolaev <a.nikolaev@xsolla.com>
2021-06-30 19:07:15 +0500

    tests config

--- xsolla-inventory-sdk/src/test/java/com/xsolla/android/inventory/TestParams.kt
+++ xsolla-inventory-sdk/src/test/java/com/xsolla/android/inventory/TestParams.kt
@@ -1,7 +1,7 @@
 package com.xsolla.android.inventory
 
 const val projectId = 53916
-const val userToken = "msp59DY6N7yn0im8zJTJt6UcOYeO50lZ"
+const val userToken = "msp5TZ5ii0EhyhkOicCDQlOw1Jx0F6II"
 
 const val subscriptionSku = "premium_access"
 const val itemForConsume = "airplane"
