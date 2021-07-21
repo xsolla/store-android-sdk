08cf13d6f019041dd01436e56842fe3234f24939
a.nikolaev <a.nikolaev@xsolla.com>
2021-07-21 16:28:35 +0500

    fix demo

--- app/src/main/java/com/xsolla/android/storesdkexample/LoginActivity.kt
+++ app/src/main/java/com/xsolla/android/storesdkexample/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.xsolla.android.storesdkexample
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -13,6 +14,17 @@ class LoginActivity : AppCompatActivity(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportFragmentManager.beginTransaction().replace(binding.root.id, AuthFragment()).commit()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // needed for google native
+        supportFragmentManager.fragments.forEach { fragment ->
+            fragment.onActivityResult(requestCode, resultCode, data)
+            fragment.childFragmentManager.fragments.forEach { childFragment ->
+                childFragment.onActivityResult(requestCode, resultCode, data)
+            }
+        }
     }
 
 }
