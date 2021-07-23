697a7f5e7daa2b0bb336357c25879000d77c7cf1
Artem Nikolaev <a.nikolaev@xsolla.com>
2021-07-23 18:35:56 +0000

    Change autotests project and implement automatic credentials substitutution

--- xsolla-inventory-sdk/src/test/java/com/xsolla/android/inventory/TestParams.kt
+++ xsolla-inventory-sdk/src/test/java/com/xsolla/android/inventory/TestParams.kt
@@ -1,7 +1,4 @@
 package com.xsolla.android.inventory
 
-const val projectId = 53916
-const val userToken = "msp5OHANPG7D77Obq06Nq4ABrRGp2G8D"
-
 const val subscriptionSku = "premium_access"
 const val itemForConsume = "airplane"
