2f17a0ea1be62d05e2e299e828867dc695691927
a.nikolaev <a.nikolaev@xsolla.com>
2021-09-28 16:12:28 +0500

    fix bundles

--- app/src/main/java/com/xsolla/android/storesdkexample/adapter/holder/VcGooglePlayViewHolder.kt
+++ app/src/main/java/com/xsolla/android/storesdkexample/adapter/holder/VcGooglePlayViewHolder.kt
@@ -4,46 +4,51 @@ import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.button.MaterialButton
+import com.xsolla.android.appcore.databinding.ItemViRealPriceBinding
 import com.xsolla.android.appcore.utils.AmountUtils
 import com.xsolla.android.store.entity.response.items.VirtualCurrencyPackageResponse
 import com.xsolla.android.storesdkexample.R
-import com.xsolla.android.storesdkexample.databinding.ItemViGooglePlayBinding
 import com.xsolla.android.storesdkexample.ui.vm.VmGooglePlay
 
 class VcGooglePlayViewHolder(
     inflater: LayoutInflater,
-    parent: ViewGroup,
+    private val parent: ViewGroup,
     private val vmGooglePay: VmGooglePlay
-) : RecyclerView.ViewHolder(inflater.inflate(R.layout.item_vi_google_play, parent, false)) {
-    private val binding = ItemViGooglePlayBinding.bind(itemView)
+) : RecyclerView.ViewHolder(inflater.inflate(R.layout.item_vi_real_price, parent, false)) {
+    private val binding = ItemViRealPriceBinding.bind(itemView)
 
     fun bind(item: VirtualCurrencyPackageResponse.Item) {
         Glide.with(itemView).load(item.imageUrl).into(binding.itemIcon)
         binding.itemName.text = item.name
         binding.itemAdditionalInfo.text = item.description
         bindItemPrice(item)
+        (binding.addToCartButton as MaterialButton).icon = ContextCompat.getDrawable(parent.context, R.drawable.ic_buy_button_icon)
     }
 
     private fun bindItemPrice(item: VirtualCurrencyPackageResponse.Item) {
         val price = item.price
         if (price!!.getAmountDecimal() == price.getAmountWithoutDiscountDecimal()) {
-            binding.itemPrice.text =
-                AmountUtils.prettyPrint(price.getAmountDecimal(), price.currency)
+            binding.itemPrice.text = AmountUtils.prettyPrint(price.getAmountDecimal(), price.currency)
             binding.itemOldPrice.visibility = View.INVISIBLE
-            binding.itemSaleLabel.visibility = View.INVISIBLE
+            // binding.itemSaleLabel.visibility = View.INVISIBLE
+            binding.itemSaleDiscount.setBackgroundColor(ContextCompat.getColor(parent.context, R.color.color_transparent))
         } else {
-            binding.itemPrice.text =
-                AmountUtils.prettyPrint(price.getAmountDecimal(), price.currency)
-            binding.itemOldPrice.text =
-                AmountUtils.prettyPrint(price.getAmountWithoutDiscountDecimal())
+            val discount = AmountUtils.calculateDiscount(price.getAmountDecimal()!!, price.getAmountWithoutDiscountDecimal()!!)
+
+            binding.itemPrice.text = AmountUtils.prettyPrint(price.getAmountDecimal(), price.currency)
+            binding.itemSaleDiscount.text = "-${discount}%"
+            binding.itemOldPrice.text = AmountUtils.prettyPrint(price.getAmountWithoutDiscountDecimal())
             binding.itemOldPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
             binding.itemOldPrice.visibility = View.VISIBLE
-            binding.itemSaleLabel.visibility = View.VISIBLE
+            //binding.itemSaleLabel.visibility = View.VISIBLE
+            binding.itemSaleDiscount.setBackgroundColor(ContextCompat.getColor(parent.context, R.color.cart_badge_color))
         }
 
-        binding.buyButton.setOnClickListener {
+        binding.addToCartButton.setOnClickListener {
             vmGooglePay.startPurchase(item.sku!!)
         }
     }
--- app/src/main/java/com/xsolla/android/storesdkexample/adapter/holder/ViGooglePlayViewHolder.kt
+++ app/src/main/java/com/xsolla/android/storesdkexample/adapter/holder/ViGooglePlayViewHolder.kt
@@ -4,22 +4,26 @@ import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.button.MaterialButton
+import com.xsolla.android.appcore.databinding.ItemViRealPriceBinding
 import com.xsolla.android.appcore.utils.AmountUtils
 import com.xsolla.android.store.entity.response.common.ExpirationPeriod
 import com.xsolla.android.storesdkexample.R
-import com.xsolla.android.storesdkexample.databinding.ItemViGooglePlayBinding
+import com.xsolla.android.storesdkexample.ui.fragments.store.ViFragmentDirections
 import com.xsolla.android.storesdkexample.ui.fragments.store.VirtualItemUiEntity
 import com.xsolla.android.storesdkexample.ui.vm.VmGooglePlay
 
 class ViGooglePlayViewHolder(
     inflater: LayoutInflater,
-    parent: ViewGroup,
+    private val parent: ViewGroup,
     private val vmGooglePay: VmGooglePlay
-) : RecyclerView.ViewHolder(inflater.inflate(R.layout.item_vi_google_play, parent, false)) {
-    private val binding = ItemViGooglePlayBinding.bind(itemView)
+) : RecyclerView.ViewHolder(inflater.inflate(R.layout.item_vi_real_price, parent, false)) {
+    private val binding = ItemViRealPriceBinding.bind(itemView)
 
     fun bind(item: VirtualItemUiEntity) {
         Glide.with(itemView).load(item.imageUrl).into(binding.itemIcon)
@@ -27,29 +31,41 @@ class ViGooglePlayViewHolder(
         bindPurchasedPlaceholderIfNeed(item)
         bindItemPrice(item)
         bindExpirationPeriod(item.inventoryOption?.expirationPeriod)
+        bindBundlePlaceholder(item)
+        (binding.addToCartButton as MaterialButton).icon = ContextCompat.getDrawable(parent.context, R.drawable.ic_buy_button_icon)
+    }
+
+    private fun bindBundlePlaceholder(item: VirtualItemUiEntity) {
+        if (item.sku == "premium_pack" || item.sku == "starter_pack"||
+            item.sku == "lootbox_pack_1" || item.sku == "lootbox_pack_2") {
+            binding.preview.visibility = View.VISIBLE
+            binding.preview.setOnClickListener {
+                it.findNavController().navigate(ViFragmentDirections.actionNavViToBundleFragment(item))
+            }
+        }
+        else{
+            binding.preview.visibility = View.INVISIBLE
+        }
     }
 
     private fun bindPurchasedPlaceholderIfNeed(item: VirtualItemUiEntity) {
         if (item.hasInInventory) {
             if (item.inventoryOption?.consumable == null && item.inventoryOption?.expirationPeriod == null) {
                 binding.purchasedPlaceholder.isVisible = true
-                binding.buyButton?.isVisible = false
-                binding.buyButton?.isVisible = false
+                binding.addToCartButton.isVisible = false
                 binding.itemPrice.isVisible = false
                 binding.itemOldPrice.isVisible = false
                 binding.itemSaleLabel.isVisible = false
             } else {
                 binding.purchasedPlaceholder.isVisible = false
-                binding.buyButton?.isVisible = true
-                binding.buyButton?.isVisible = true
+                binding.addToCartButton.isVisible = true
                 binding.itemPrice.isVisible = true
                 binding.itemOldPrice.isVisible = true
                 binding.itemSaleLabel.isVisible = true
             }
         } else {
             binding.purchasedPlaceholder.isVisible = false
-            binding.buyButton?.isVisible = true
-            binding.buyButton?.isVisible = true
+            binding.addToCartButton.isVisible = true
             binding.itemPrice.isVisible = true
             binding.itemOldPrice.isVisible = true
             binding.itemSaleLabel.isVisible = true
@@ -64,14 +80,20 @@ class ViGooglePlayViewHolder(
             binding.itemSaleLabel.visibility = View.INVISIBLE
         } else {
             if (binding.itemOldPrice.isVisible && binding.itemSaleLabel.isVisible) {
+                val discount = AmountUtils.calculateDiscount(price.getAmountDecimal()!!, price.getAmountWithoutDiscountDecimal()!!)
+
                 binding.itemPrice.text = AmountUtils.prettyPrint(price.getAmountDecimal(), price.currency)
+                binding.itemSaleDiscount.text = "-${discount}%"
                 binding.itemOldPrice.text = AmountUtils.prettyPrint(price.getAmountWithoutDiscountDecimal())
                 binding.itemOldPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
             }
         }
 
-        binding.buyButton.setOnClickListener {
-            vmGooglePay.startPurchase(item.sku!!, item.virtualItemType!!)
+        binding.addToCartButton.setOnClickListener {
+            when (item.virtualItemType) {
+                null -> vmGooglePay.startPurchase(item.sku!!) // bundle
+                else -> vmGooglePay.startPurchase(item.sku!!, item.virtualItemType)
+            }
         }
     }
 
