1ad69e4915a07cf13fb3d83d6e192ec69ebdf2f3
Artem Nikolaev <a.nikolaev@xsolla.com>
2021-12-27 18:23:49 +0500

    autosetup in payments sdk

--- xsolla-payments-sdk/src/main/java/com/xsolla/android/payments/ui/ActivityPaystation.kt
+++ xsolla-payments-sdk/src/main/java/com/xsolla/android/payments/ui/ActivityPaystation.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 abstract class ActivityPaystation : AppCompatActivity() {
     companion object {
-        const val ARG_URL = "token"
+        const val ARG_URL = "url"
+        const val ARG_REDIRECT_SCHEME = "redirect_scheme"
+        const val ARG_REDIRECT_HOST = "redirect_host"
 
         const val RESULT = "result"
     }
