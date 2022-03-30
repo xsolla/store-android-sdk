6592628f4ca390d15e4c808b43efecfecd4f50a3
Artem Nikolaev <a.nikolaev@xsolla.com>
2022-03-30 18:50:36 +0800

    refactor login tests, add oauth token refreshing test

---
+++ xsolla-login-sdk/src/test/java/com/xsolla/android/login/RegistrationTests.kt
@@ -0,0 +1,70 @@
+package com.xsolla.android.login
+
+import com.xsolla.android.login.callback.RegisterCallback
+import com.xsolla.android.login.util.TestUtils.initLoggedOut
+import com.xsolla.android.login.util.TestUtils.initSdkJwt
+import com.xsolla.android.login.util.TestUtils.initSdkOauth
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.util.*
+import java.util.concurrent.CountDownLatch
+
+@RunWith(RobolectricTestRunner::class)
+class RegistrationTests {
+
+    //TODO check payload, acceptConsent, promoEmailAgreement and fail scenarios
+    @Test
+    fun registerJwt_Success() {
+        initSdkJwt()
+        initLoggedOut()
+
+        val latch = CountDownLatch(1)
+        var error = false
+        XLogin.register(
+            UUID.randomUUID().toString(),
+            "${UUID.randomUUID()}@gmail.com",
+            UUID.randomUUID().toString(),
+            object : RegisterCallback {
+                override fun onSuccess() {
+                    latch.countDown()
+                }
+
+                override fun onError(throwable: Throwable?, errorMessage: String?) {
+                    error = true
+                    latch.countDown()
+                }
+            }
+        )
+        latch.await()
+        Assert.assertFalse(error)
+    }
+
+    @Test
+    fun registerOauth_Success() {
+        initSdkOauth()
+        initLoggedOut()
+
+        val latch = CountDownLatch(1)
+        var error = false
+        XLogin.register(
+            UUID.randomUUID().toString(),
+            "${UUID.randomUUID()}@gmail.com",
+            UUID.randomUUID().toString(),
+            object : RegisterCallback {
+                override fun onSuccess() {
+                    latch.countDown()
+                }
+
+                override fun onError(throwable: Throwable?, errorMessage: String?) {
+                    error = true
+                    latch.countDown()
+                }
+            }
+        )
+        latch.await()
+        Assert.assertFalse(error)
+    }
+
+}
---
+++ xsolla-login-sdk/src/test/java/com/xsolla/android/login/util/TestUtils.kt
@@ -0,0 +1,79 @@
+package com.xsolla.android.login.util
+
+import android.content.Context
+import android.provider.Settings
+import androidx.test.core.app.ApplicationProvider
+import com.xsolla.android.login.*
+import com.xsolla.android.login.callback.AuthCallback
+import com.xsolla.android.login.callback.AuthViaDeviceIdCallback
+import org.junit.Assert
+import java.util.concurrent.CountDownLatch
+
+object TestUtils {
+
+    // Init methods for different auth types
+
+    fun initSdkOauth() {
+        val loginConfig = LoginConfig.OauthBuilder()
+            .setProjectId(projectId)
+            .setOauthClientId(oauthClientId)
+            .setRedirectUriScheme("app")
+            .setRedirectUriHost("example")
+            .build()
+        XLogin.init(ApplicationProvider.getApplicationContext(), loginConfig)
+    }
+
+    fun initSdkJwt() {
+        val loginConfig = LoginConfig.JwtBuilder()
+            .setProjectId(projectId)
+            .setRedirectUriScheme("app")
+            .setRedirectUriHost("example")
+            .build()
+        XLogin.init(ApplicationProvider.getApplicationContext(), loginConfig)
+    }
+
+    // Init for different auth states
+
+    fun initLoggedOut() {
+        XLogin.logout()
+    }
+
+    fun initLoggedInByDeviceId() {
+        XLogin.logout()
+        val context: Context = ApplicationProvider.getApplicationContext()
+        Settings.Secure.putString(context.contentResolver, Settings.Secure.ANDROID_ID, deviceId)
+        val latch = CountDownLatch(1)
+        var error = false
+        XLogin.authenticateViaDeviceId(object : AuthViaDeviceIdCallback {
+            override fun onSuccess() {
+                latch.countDown()
+            }
+
+            override fun onError(throwable: Throwable?, errorMessage: String?) {
+                error = true
+                latch.countDown()
+            }
+        })
+        latch.await()
+        Assert.assertFalse(error)
+    }
+
+    fun initLoggedInByPassword() {
+        XLogin.logout()
+        val latch = CountDownLatch(1)
+        var error = false
+        XLogin.login(username, password, object : AuthCallback {
+            override fun onSuccess() {
+                latch.countDown()
+            }
+
+            override fun onError(throwable: Throwable?, errorMessage: String?) {
+                error = true
+                latch.countDown()
+            }
+        })
+        latch.await()
+        Assert.assertFalse(error)
+    }
+
+}
