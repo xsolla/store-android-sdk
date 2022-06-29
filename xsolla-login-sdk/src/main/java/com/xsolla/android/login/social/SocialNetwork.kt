b4ae1074154b9a21575a6e42df5dcf012ed31860
Artem Nikolaev <a.nikolaev@xsolla.com>
2022-06-29 13:16:21 +0800

    cleanup

--- xsolla-login-sdk/src/main/java/com/xsolla/android/login/social/SocialNetwork.kt
+++ xsolla-login-sdk/src/main/java/com/xsolla/android/login/social/SocialNetwork.kt
@@ -1,34 +1,34 @@
 package com.xsolla.android.login.social
 
 enum class SocialNetwork(val providerName: String) {
-    GOOGLE("google"),//+
-    FACEBOOK("facebook"),//+
-    TWITTER("twitter"),//+
-    LINKEDIN("linkedin"),//+
-    NAVER("naver"),//+
-    BAIDU("baidu"),//+
-    AMAZON("amazon"),//+
-    APPLE("apple"),//+
-    BATTLENET("battlenet"),//+
-    DISCORD("discord"),//+
-    GITHUB("github"),//+
-    KAKAO("kakao"),//+
+    GOOGLE("google"),
+    FACEBOOK("facebook"),
+    TWITTER("twitter"),
+    LINKEDIN("linkedin"),
+    NAVER("naver"),
+    BAIDU("baidu"),
+    AMAZON("amazon"),
+    APPLE("apple"),
+    BATTLENET("battlenet"),
+    DISCORD("discord"),
+    GITHUB("github"),
+    KAKAO("kakao"),
     MAILRU("mailru"),
     MICROSOFT("microsoft"),
-    MSN("msn"),//+
-    OK("ok"),//+
-    PAYPAL("paypal"),//+
-    PSN("psn"), // nonono
-    QQ("qq"),//+
-    REDDIT("reddit"),//+
-    STEAM("steam"),//+
-    TWITCH("twitch"),//+
-    VIMEO("vimeo"),//+
-    VK("vk"),//+
-    WECHAT("wechat"),//+
-    WEIBO("weibo"),//+
-    YAHOO("yahoo"),//+
-    YANDEX("yandex"),//+
-    YOUTUBE("youtube"),//+
-    XBOX("xbox");//+
+    MSN("msn"),
+    OK("ok"),
+    PAYPAL("paypal"),
+    PSN("psn"),
+    QQ("qq"),
+    REDDIT("reddit"),
+    STEAM("steam"),
+    TWITCH("twitch"),
+    VIMEO("vimeo"),
+    VK("vk"),
+    WECHAT("wechat"),
+    WEIBO("weibo"),
+    YAHOO("yahoo"),
+    YANDEX("yandex"),
+    YOUTUBE("youtube"),
+    XBOX("xbox");
 }
