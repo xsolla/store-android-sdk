2a0482d257b0b073ac918802054915d3b39503bd
Artem Nikolaev <a.nikolaev@xsolla.com>
2022-06-30 13:50:43 +0800

    remove separate enum for linkable social networks

--- xsolla-login-sdk/src/main/java/com/xsolla/android/login/entity/response/LinkedSocialNetworkResponse.kt
+++ xsolla-login-sdk/src/main/java/com/xsolla/android/login/entity/response/LinkedSocialNetworkResponse.kt
@@ -1,14 +1,21 @@
 package com.xsolla.android.login.entity.response
 
 import com.google.gson.annotations.SerializedName
-import com.xsolla.android.login.social.SocialNetworkForLinking
+import com.xsolla.android.login.social.SocialNetwork
 
 data class LinkedSocialNetworkResponse(
-        @SerializedName("full_name")
-        val fullName: String?,
-        val nickname: String?,
-        val picture: String?,
-        val provider: SocialNetworkForLinking?,
-        @SerializedName("social_id")
-        val socialId: String
+    @SerializedName("full_name")
+    val fullName: String?,
+
+    @SerializedName("nickname")
+    val nickname: String?,
+
+    @SerializedName("picture")
+    val picture: String?,
+
+    @SerializedName("provider")
+    val socialNetwork: SocialNetwork?,
+
+    @SerializedName("social_id")
+    val socialId: String
 )
