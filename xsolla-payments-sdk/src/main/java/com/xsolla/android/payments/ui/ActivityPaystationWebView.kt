28afec05148b13bb72a61a5af68f697a2481c52f
Olga Salina <o.salina@xsolla.com>
2023-12-20 19:23:24 +0300

    SDK-3457 Add WebView support for file downloading and alipay redirects

--- xsolla-payments-sdk/src/main/java/com/xsolla/android/payments/ui/ActivityPaystationWebView.kt
+++ xsolla-payments-sdk/src/main/java/com/xsolla/android/payments/ui/ActivityPaystationWebView.kt
@@ -2,14 +2,18 @@ package com.xsolla.android.payments.ui
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.DownloadManager
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
+import android.webkit.URLUtil
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.xsolla.android.payments.R
 import com.xsolla.android.payments.XPayments
+import java.lang.Exception
 
 internal class ActivityPaystationWebView : ActivityPaystation() {
 
@@ -45,12 +49,29 @@ internal class ActivityPaystationWebView : ActivityPaystation() {
     @SuppressLint("SetJavaScriptEnabled")
     private fun configureWebView() {
         webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.loadsImagesAutomatically = true
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(webView: WebView, url: String): Boolean {
                 val urlLower = url.lowercase()
 
                 if (urlLower.startsWith(redirectScheme))
                     return false
+
+                if (urlLower.startsWith("alipays")) {
+                    try {
+                        val browserIntent = Intent()
+                            .setAction(Intent.ACTION_VIEW)
+                            .addCategory(Intent.CATEGORY_BROWSABLE)
+                            .setData(Uri.parse(url))
+                        webView.context.startActivity(browserIntent)
+                        return true
+                    } catch (e: Exception) {
+                        return false
+                    }
+                }
+
 
                 if (!(urlLower.startsWith("https:") || urlLower.startsWith("http:")))
                     return true
@@ -81,6 +102,24 @@ internal class ActivityPaystationWebView : ActivityPaystation() {
                 super.doUpdateVisitedHistory(view, url, isReload)
             }
 
+        }
+        webView.setDownloadListener { url, userAgent, contentDisposition, mimeType, _ ->
+
+            val request = DownloadManager.Request(Uri.parse(url))
+            request.setMimeType(mimeType)
+            request.addRequestHeader("User-Agent", userAgent)
+            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalPublicDir(
+
+                Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+
+                    url, contentDisposition, mimeType
+
+                )
+            )
+            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
         }
     }
 
